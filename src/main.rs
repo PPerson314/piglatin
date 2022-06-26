fd86dacb0a67c6f28774d7c93ec81da4644dd35c
PPerson <paul.l.vantilburg@gmail.com>
2022-06-26 13:41:26 +0200

    removed vowel array and use starts_with() instead

--- src/main.rs
+++ src/main.rs
@@ -12,8 +12,7 @@ fn main() {
 fn pig_latin(s: String) -> String {
     let rest_of_word = &s[1..];
     let first_letter = &s[0..1];
-    let vowels = ["a", "e", "i", "o", "u"];
-    if vowels.contains(&first_letter) {
+    if s.starts_with("a") || s.starts_with("e") || s.starts_with("i") || s.starts_with("o") || s.starts_with("u") {
         format!("{}-hay", s)
     } else {
         format!("{}-{}ay", rest_of_word, first_letter)
